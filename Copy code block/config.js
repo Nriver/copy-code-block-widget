03c69dca28ab3e4c5a54ec40a9b9f679f293a025
Nriver <6752679+Nriver@users.noreply.github.com>
2025-03-28 09:36:46 +0800

    update comment

--- Copy code block/config.js
+++ Copy code block/config.js
@@ -1,6 +1,7 @@
 // Language name. Change language name to anyone you like in the translations file. For example, 'cn' for Chinese, 'en' for english.
 var lang = 'en';
 
+// If it doesn’t work in the browser, try increasing the delay to better suit your settings.
 var executeDelay = 200;
 
 module.exports = {
